547047fdc8db07c5c5f6f934cd121c26c98ac0dd
the_sky sharma <sharmaakash11401!gmail.com>
2026-05-19 14:30:07 +0530

    changes made

--- data.js
+++ data.js
@@ -1,14 +1,15 @@
 const data = [
+    "wixford.contact@gmail.com",
+    "surbhia.infowind@gmail.com",
+    "contact@acadifysolution.com",
+    "hr@codeedextechnologies.com",
+    "hiring@servicesats.com",
+    "shiva.official1964@gmail.com",
     "nezuware.info@gmail.com",
-    "hr@codegeniusinnovations.in",
-    "ankitgkv4933@gmail.com",
-    "chaitanya@celerisventures.ai",
-    "hr@codegeniusinnovations.in",
-    "ankitgkv4933@gmail.com",
-    "arushsingh3146@gmail.com",
-    "hrfindonehrsolutions@gmail.com",
-    "Career@gitakshmi.com",
-    "saloni.oberoi@simplifyingskills.com"
+    "hrvrinda076@gmail.com",
+    "subhika.chatterjee@quokkalabs.com",
+    "anisha.sharma@mobulous.com",
+    
     
 
 ];
